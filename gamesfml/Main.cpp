1eb284cb6b89eeec38ec3a46598120e92e15d083
viperzx <antonio.m.goncalves.pereira@hotmail.com>
2020-07-25 21:27:45 +0100

    Main cpp created

---
+++ gamesfml/Main.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+
+
+
+int main()
+{
+	//Window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My first game", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event event;
+
+	//Game loop
+	while (window.isOpen()) {
+		//Event pooling
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+	}
+
+	//Update
+
+	//Render
+	window.clear(sf::Color::Blue); // Clear old frame
+
+}
